7333e1cf0c07f3cda16a740228c46a34d691487d
GiorgosSimos <giorgos_Simos@windowslive.com>
2023-10-10 13:34:51 +0300

    - Merged changes from enhanced_devices_branch and trojanHorse_devices_branch into devices_branch

--- ZeusPalace/Program.cs
+++ ZeusPalace/Program.cs
@@ -17,7 +17,7 @@ namespace ZeusPalace
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TrojanHorseDevicesForm());
+            Application.Run(new AppControllerForm());
         }
     }
 }
